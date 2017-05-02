fcbf9d2fdc3f13a6fdcaaca88d83755276fa41a9
rnburn <contact@rnburn.com>
2017-05-02 12:30:09 -0400

    Updated to work with opentracing 0.14.0

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,16 @@
-import tracer from "opentracing";
+import * as opentracing from "opentracing";
 import * as url from "url";
 
 export default function middleware(options = {}) {
   if (options.tracer) {
-    tracer.initGlobalTracer(options.tracer);
+    opentracing.initGlobalTracer(options.tracer);
   } else {
-    tracer.initGlobalTracer();
+    opentracing.initGlobalTracer();
   }
 
   return (req, res, next) => {
-    const wireCtx = tracer.extract(tracer.FORMAT_HTTP_HEADERS, req.headers);
+    const tracer = opentracing.globalTracer();
+    const wireCtx = tracer.extract(opentracing.FORMAT_HTTP_HEADERS, req.headers);
     const pathname = url.parse(req.url).pathname;
     const span = tracer.startSpan(pathname, {childOf: wireCtx});
     span.logEvent("request_received");
@@ -22,7 +23,7 @@ export default function middleware(options = {}) {
     // include trace ID in headers so that we can debug slow requests we see in
     // the browser by looking up the trace ID found in response headers
     const responseHeaders = {};
-    tracer.inject(span, tracer.FORMAT_TEXT_MAP, responseHeaders);
+    tracer.inject(span, opentracing.FORMAT_TEXT_MAP, responseHeaders);
     Object.keys(responseHeaders).forEach(key => res.setHeader(key, responseHeaders[key]));
 
     // add the span to the request object for handlers to use
